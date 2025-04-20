63da30012896a8dea82a041d844f45496a42b33f
Lorenzo Cella <lorenzo@livingdocs.io>
2025-04-20 09:31:51 +0200

    add dark theme controller and home link

--- app.vue
+++ app.vue
@@ -7,13 +7,27 @@
       <div class="drawer-content">
         <!-- Page content -->
         <div class="flex flex-col">
-          <div class="flex justify-start p-4">
-            <label
-              for="sidebar-drawer"
-              class="btn btn-square btn-ghost drawer-button lg:hidden"
-            >
-              <IconsBurger class="w-6 h-6" />
-            </label>
+          <div class="flex justify-between p-4">
+            <div class="flex gap-2">
+              <label
+                for="sidebar-drawer"
+                class="btn btn-square btn-ghost drawer-button lg:hidden"
+              >
+                <IconsBurger class="w-6 h-6" />
+              </label>
+            </div>
+            <div class="flex gap-2">
+              <label class="swap swap-rotate">
+                <!-- this hidden checkbox controls the state -->
+                <input
+                  type="checkbox"
+                  class="theme-controller"
+                  value="halloween"
+                />
+                <IconsSun class="w-6 h-6" />
+                <IconsMoon class="w-6 h-6" />
+              </label>
+            </div>
           </div>
 
           <main class="flex-1 p-8 lg:pl-8">
@@ -27,7 +41,11 @@
         <label for="sidebar-drawer" class="drawer-overlay"></label>
         <aside class="w-80 min-h-screen bg-base-100">
           <div class="p-4 border-b border-base-300">
-            <h1 class="text-center text-xl font-bold">My Reading Notes</h1>
+            <h1 class="text-center font-serif text-xl font-bold">
+              <NuxtLink to="/" class="link link-hover">
+                My Reading Notes
+              </NuxtLink>
+            </h1>
           </div>
           <div class="overflow-y-auto h-[calc(100vh-4rem)]">
             <BookList />
